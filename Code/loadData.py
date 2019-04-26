d81b61347c417495587874985e76bc8981b89e69
patelni14 <patel.n@wustl.edu>
2019-04-26 09:53:03 -0500

    Add files via upload

---
+++ Code/loadData.py
@@ -0,0 +1,217 @@
+import numpy as np
+import pandas as pd
+import argparse
+import sys
+import csv
+import os
+import scipy.stats as ss
+# from collections import OrderedDict
+
+
+def createNumpyArray(dir):
+	dataFile = open(dir+'Data.csv', 'r')
+	GenesFile = open(dir+'GeneNames.csv', 'r')
+	TFsFile = open(dir+'TFNames.csv', 'r')
+
+	dataReader = csv.reader(dataFile)
+	dataData = []
+	for row in dataReader:
+		dataData.append( [ float(elem) for elem in row ] )
+
+	GenesReader = csv.reader(GenesFile)
+	GenesData = []
+	for row in GenesReader:
+		GenesData.append( [ str.strip(str(elem)) for elem in row ] )
+
+	TFsReader = csv.reader(TFsFile)
+	TFsData = []
+	for row in TFsReader:
+		TFsData.append( [ str(elem) for elem in row ][0] )
+
+	return(dataData,GenesData,TFsData)
+
+def createSysDict(namesFile):
+	with open(namesFile, mode='r') as infile:
+		reader = csv.reader(infile)
+		mydict = {rows[1]:rows[0] for rows in reader}
+	return mydict
+
+def parseData(locIn,locOut,colToKeep=1,charsToDrop=7):
+	DataData = [];
+	GenesData = [];
+	TFsData = [];
+
+	count = 0
+	for filename in os.listdir(locIn):
+		print(filename)
+		with open(locIn+"/"+filename) as inf:
+			reader = csv.reader(inf, delimiter="\t")
+			data = list(zip(*reader))
+			genes = data[0]
+			values = data[colToKeep]
+			tfName = filename[:-charsToDrop]
+		DataData.append(values[1:])
+		GenesData.append(genes[1:])
+		TFsData.append(tfName)
+		count = count+1
+
+	# otherLoc = '/Users/patelni/WolframWorkspaces/Base/ThresholdAnalysis/Python Code/Data/Yeast/Kemmeren'
+	saveData(locOut, DataData, GenesData, TFsData)
+	
+def parseENCODEshRNA(locIn,locOut):
+	parseKemmeren(locIn,locOut)
+
+def parseENCODEChIP(locIn,locOut):
+	pValsData = [];
+	LFCsData = [];
+	GenesData = [];
+	TFsData = [];
+
+	count = 0
+	for filename in os.listdir(locIn):
+		print(filename)
+		with open(locIn+"/"+filename) as inf:
+			reader = csv.reader(inf, delimiter="\t")
+			data = list(zip(*reader))
+			genes = data[0]
+			pvals = data[3]
+			lfcs = data[3]
+			tfName = filename[:-16]
+
+		uniqueGenes = list(set(genes[1:]))
+		genesNP = np.array(genes)
+		pvalsNP = np.array(pvals)
+		newPValsList = []
+
+		for gene in uniqueGenes:
+			geneRowNums = np.where(genesNP == gene)[0]
+			genePvals = pvalsNP[geneRowNums]
+			newPval = 0
+			# print(genePvals)
+			for pval in genePvals:
+				if(float(pval) > 1): #checks to see if the -log10(pval) is greater than 1 (or that the pvalue is less than 0.1)
+					newPval+=float(pval)
+
+			newPval = 10**(-newPval)
+			newPValsList.append(newPval)
+
+		pValsData.append(newPValsList)
+		LFCsData.append(newPValsList)
+		GenesData.append(uniqueGenes)
+		TFsData.append(tfName)
+		count = count+1
+
+	# otherLoc = '/Users/patelni/WolframWorkspaces/Base/ThresholdAnalysis/Python Code/Data/Yeast/Kemmeren'
+	saveData(locOut, pValsData, LFCsData, GenesData, TFsData)
+
+
+def saveData(loc, DataData, GenesData, TFsData):
+	with open(loc+"/"+'Data.csv', 'w') as writeFile:
+		writer = csv.writer(writeFile)
+		writer.writerows(DataData)
+	with open(loc+"/"+'GeneNames.csv', 'w') as writeFile:
+		writer = csv.writer(writeFile)
+		writer.writerows(GenesData)
+	with open(loc+"/"+'TFNames.csv', 'w') as writeFile:
+		writer = csv.writer(writeFile)
+		for val in TFsData:
+			writer.writerow([val])
+
+def splitFiles(loc):
+	DataFile = open(loc+'/Data.csv', 'r')
+	GenesFile = open(loc+'/GeneNames.csv', 'r')
+	TFsFile = open(loc+'/TFNames.csv', 'r')
+
+	DataReader = csv.reader(DataFile)
+	GenesReader = csv.reader(GenesFile)
+	TFsReader = csv.reader(TFsFile)
+
+	# ordered_fieldnames = OrderedDict([('field1',None),('field2',None)])
+
+	for row in TFsReader:
+		TF = [ str(elem) for elem in row ][0]
+		with open(loc+"/Split Files/"+TF + '.txt', 'w') as writeFile:
+			writer = csv.writer(writeFile, delimiter = '\t')
+			writer.writerow(['#genes','#PVal']) 
+			genes = next(GenesReader)
+			values = next(DataReader)
+			for i in range(len(genes)):
+				writer.writerow([genes[i],values[i]]) 
+
+def checkInsufficientTFs(loc):
+	DataData,GenesData,TFsData = createNumpyArray(loc)
+	insuffTFs = np.zeros(len(TFsData))
+
+	for i in range(len(TFsData)):
+		TF = TFsData[i]
+		Genes = GenesData[i]
+		Data = DataData[i]
+
+		if TF in list(Genes) and Data[list(Genes).index(TF)] > np.log2(0.5):
+			insuffTFs[i] = 1
+
+	return insuffTFs
+
+def removeInsufficientTFs(loc):
+	insuffTFs = checkInsufficientTFs(loc)
+
+	DataData = []
+	GenesData = []
+	TFsData = []
+
+	DataFile = open(loc+'/Data.csv', 'r')
+	GenesFile = open(loc+'/GeneNames.csv', 'r')
+	TFsFile = open(loc+'/TFNames.csv', 'r')
+
+	DataReader = csv.reader(DataFile)
+	GenesReader = csv.reader(GenesFile)
+	TFsReader = csv.reader(TFsFile)
+
+	for i in range(len(insuffTFs)):
+		DataRow = next(DataReader)
+		GenesRow = next(GenesReader)
+		TFsRow = next(TFsReader)
+		if insuffTFs[i] == 0:
+			DataData.append( [ float(elem) for elem in DataRow ] )
+			GenesData.append( [ str(elem) for elem in GenesRow ] )
+			TFsData.append( [ str(elem) for elem in TFsRow ][0] )
+
+	otherLoc = '/Users/patelni/WolframWorkspaces/Base/ThresholdAnalysis/Python Code/Data/Yeast/Kemmeren'
+	saveData(otherLoc, DataData, GenesData, TFsData)
+
+def computeTopNEdges(locIn,locOut,numEdges):
+	dataFile = np.loadtxt(locIn)
+	dataFileAbs = np.absolute(dataFile)
+	rankedFile = rankEdges(dataFileAbs)
+	mask = (rankedFile <= numEdges)*1
+	thresholdedEdges = dataFile*mask
+
+	with open(locOut+"/"+'Data.csv', 'w') as writeFile:
+		writer = csv.writer(writeFile)
+		writer.writerows(thresholdedEdges)
+
+def rankEdges(data):
+	m, n = np.shape(data)
+	rank = (m*n+1 - ss.rankdata(data)).reshape((m,n))
+	return rank
+
+
+def main(argv):
+	# computeTopNEdges('Input Directory Path','Output Directory Path',Number of Edges)
+	# parseData('Input Directory Path','Output Directory Path',Column to Keep,Number of Characters to Drop at end of file name):
+	computeTopNEdges('/scratch/mblab/pateln/ThresholdAnalysis_Python/ToPost/RawData/NP_ZEV_15_45_90/netprophet2_network.adjmtr','/scratch/mblab/pateln/ThresholdAnalysis_Python/ToPost/Data/NetProphet_Z_15_45_90_150K/',150000)
+
+if __name__ == "__main__":
+	main(sys.argv)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Code/runDualThreshold.py
@@ -0,0 +1,234 @@
+import numpy as np
+import pandas as pd
+import argparse
+import random
+import sys
+sys.path.append("/scratch/mblab/pateln/ThresholdAnalysis_Python/Code")
+from loadData import *
+from statistics import *
+from thresholdSearch import *
+from scipy.stats import hypergeom,spearmanr
+from datetime import datetime
+
+np.set_printoptions(threshold=np.nan)
+
+
+def parse_args(argv):
+  parser = argparse.ArgumentParser(description="")
+  parser.add_argument("-d","--de_dir")
+  parser.add_argument("-b","--bin_dir")
+  parser.add_argument("-r","--random",default = False)
+  parser.add_argument("-g","--geneNames_file",default = "/scratch/mblab/pateln/ThresholdAnalysis_Python/ExtraFiles/YeastCommonAndSystematicGeneNames.csv")
+  parser.add_argument("-s","--incl_spearman",default = False)
+  parser.add_argument("-n","--TF_num")
+  parser.add_argument("-o","--opt_crit",default = "pval")
+  parser.add_argument("-w","--rank_width",default = 1.01)
+  parser.add_argument("-i","--iter_num",default = "")
+  parser.add_argument("-l","--tf_list",default = "")
+  parser.add_argument("-u","--genes_universe",default = "")
+  parser.add_argument("-a","--organism",default = "yeast")
+  parser.add_argument("-j","--DE_decreasing",default = True)
+  parser.add_argument("-k","--Bin_decreasing",default = True)
+  parsed = parser.parse_args(argv[1:])
+  return parsed
+
+def runDualThresholds(DEData,BinData,TFIntersection,TFNum,GenesUniverse_Given):
+	global sysDict,parsed
+
+	if GenesUniverse_Given!=None:
+		GenesUniverse = GenesUniverse_Given
+
+	TF = TFIntersection[int(TFNum)]
+	print(TF)
+	sys.stdout.flush()
+	BinDataData,BinGenesData,BinTFsData = BinData
+	DEDataData,DEGenesData,DETFsData = DEData
+	
+	BinTFIndex = BinTFsData.index(TF)
+	DETFIndex = BinTFsData.index(TF)
+	DEData,BinData = alignToUniverse(DEData,BinData,GenesUniverse,BinTFIndex,DETFIndex,str2Bool(parsed.Bin_decreasing))
+	BinDataData,BinGenesData,BinTFsData = BinData
+	DEDataData,DEGenesData,DETFsData = DEData
+
+	if parsed.random:
+		DEValues,DEGenes = sortDataRandom(DEDataData[DETFsData.index(TF)], DEGenesData[DETFsData.index(TF)])
+	else:
+		DEValues,DEGenes = sortData(DEDataData[DETFsData.index(TF)], DEGenesData[DETFsData.index(TF)],str2Bool(parsed.DE_decreasing))
+	
+	BinValues,BinGenes = sortData(BinDataData[BinTFsData.index(TF)], BinGenesData[BinTFsData.index(TF)],str2Bool(parsed.Bin_decreasing))
+
+	if GenesUniverse_Given==None:
+		GenesUniverse = computeUniverse(DEGenes,BinGenes)
+
+	DEValues,DEGenes = getSubset(DEGenes,DEValues,GenesUniverse)
+	BinValues,BinGenes = getSubset(BinGenes,BinValues,GenesUniverse)
+
+	optimizedResults = optimizedThresholds(TF,BinGenes,BinValues,DEGenes,DEValues,GenesUniverse)
+
+	if parsed.random:
+		fileName = TF + "_" + parsed.iter_num + ".csv"
+	else:
+		fileName = TF + ".csv"
+
+	with open(fileName,'w') as resultFile:
+		wr = csv.writer(resultFile)
+		print(optimizedResults)
+		wr.writerow(optimizedResults)
+
+def optimizedThresholds(TF,BinGenes,BinValues,DEGenes,DEValues,GenesUniverse):
+	global sysDict
+	if parsed.organism == "yeast":
+		TFCommon = sysDict[TF]
+	else:
+		TFCommon = TF
+
+	if str2Bool(parsed.DE_decreasing) == True:
+		DErankList = generateRanks(len(DEValues),float(parsed.rank_width),DEValues,0,str2Bool(parsed.DE_decreasing))
+	else:
+		if parsed.organism == "yeast":
+			DErankList = generateRanks(len(DEValues),float(parsed.rank_width),DEValues,1,str2Bool(parsed.DE_decreasing))
+		else:
+			DErankList = generateRanks(len(DEValues),float(parsed.rank_width),DEValues,0.1,str2Bool(parsed.DE_decreasing))
+	
+	if str2Bool(parsed.Bin_decreasing) == True:
+		binRankList = generateRanks(len(BinValues),float(parsed.rank_width),BinValues,0,str2Bool(parsed.Bin_decreasing))
+	else:
+		binRankList = generateRanks(len(BinValues),float(parsed.rank_width),BinValues,0.1,str2Bool(parsed.Bin_decreasing))
+
+	bestFDR = 1
+	bestPVal = 1
+	bestFE = 0
+	bestRR = 0
+	bestRRsk = 0
+	bestSC = 0
+	bestSPVal = 1
+	bestJS = 0
+	bestIntersection = 0
+	boundSubGenes = []
+	DESubGenes = []
+
+	if len(DErankList) > 0:
+		bestDEThresh = DErankList[0]
+	else:
+		bestDEThresh = 0
+	if len(binRankList) > 0:
+		bestBinThresh = binRankList[0]
+	else:
+		bestBinThresh = 0
+
+	print('DE list = ',DErankList)
+	print('bin list = ',binRankList)
+	sys.stdout.flush()
+
+	for i in range(len(binRankList)):
+		binThresh = binRankList[i]
+		boundSubGenes = BinGenes[:binThresh]
+		boundSubData = BinValues[:binThresh]
+		for j in range(len(DErankList)):
+			DEThresh = DErankList[j]
+			DESubGenes = DEGenes[:DEThresh]
+			DESubData = DEValues[:DEThresh]
+			GenesIntersection = list(set(DESubGenes) & set(boundSubGenes))
+			if len(GenesIntersection) > 0:
+				FDRBound = computeFDRLowerBound(GenesIntersection,GenesUniverse,DESubGenes,boundSubGenes)
+				hyperPval = computeHyperPVal(GenesIntersection,GenesUniverse,DESubGenes,boundSubGenes)
+				responseRate = computeResponseRate(GenesIntersection,GenesUniverse,DESubGenes,boundSubGenes)
+				foldEnrichment = computeFoldEnrichment(GenesIntersection,GenesUniverse,DESubGenes,boundSubGenes)
+				relativeRisk = computeRelativeRisk(GenesIntersection,GenesUniverse,DESubGenes,boundSubGenes)
+				# rho,spearmanPval = computeSpearman(boundSubPVals,boundSubGenes,DESubPVals,DESubGenes,GenesIntersection)
+				rho,spearmanPval = [0,1]
+				jaccardSim = computeJaccardSimilarity(DESubGenes,boundSubGenes)
+
+
+				optCrit = parsed.opt_crit
+				if optCrit=="fdr":
+					if FDRBound < bestFDR and len(GenesIntersection) > 1:
+						bestFDR,bestIntersection,bestPVal,bestFE,bestRR,bestRRsk,bestSC,bestSPVal,bestJS = [FDRBound,len(GenesIntersection),hyperPval,foldEnrichment,responseRate,relativeRisk,rho,spearmanPval,jaccardSim]
+						bestBinThresh = binThresh
+						bestDEThresh = DEThresh
+					if FDRBound == bestFDR and hyperPval < bestPVal and len(GenesIntersection) > 1:
+						bestFDR,bestIntersection,bestPVal,bestFE,bestRR,bestRRsk,bestSC,bestSPVal,bestJS = [FDRBound,len(GenesIntersection),hyperPval,foldEnrichment,responseRate,relativeRisk,rho,spearmanPval,jaccardSim]
+						bestBinThresh = binThresh
+						bestDEThresh = DEThresh
+					# if FDRBound == bestFDR and len(GenesIntersection) > bestIntersection and len(GenesIntersection) > 1:
+					# 	bestFDR,bestIntersection,bestPVal,bestFE,bestRR,bestRRsk,bestSC,bestSPVal,bestJS = [FDRBound,len(GenesIntersection),hyperPval,foldEnrichment,responseRate,relativeRisk,rho,spearmanPval,jaccardSim]
+					# 	bestBinThresh = binThresh
+					# 	bestDEThresh = DEThresh
+				if optCrit=="pval":
+					if hyperPval < bestPVal and len(GenesIntersection) > 1:
+						bestFDR,bestIntersection,bestPVal,bestFE,bestRR,bestRRsk,bestSC,bestSPVal,bestJS = [FDRBound,len(GenesIntersection),hyperPval,foldEnrichment,responseRate,relativeRisk,rho,spearmanPval,jaccardSim]
+						bestBinThresh = binThresh
+						bestDEThresh = DEThresh
+				elif optCrit=="fe":
+					if foldEnrichment > bestFE and len(GenesIntersection) > 1:
+						bestFDR,bestIntersection,bestPVal,bestFE,bestRR,bestRRsk,bestSC,bestSPVal,bestJS = [FDRBound,len(GenesIntersection),hyperPval,foldEnrichment,responseRate,relativeRisk,rho,spearmanPval,jaccardSim]
+						bestBinThresh = binThresh
+						bestDEThresh = DEThresh
+				elif optCrit=="rr":
+					if responseRate > bestRR and len(GenesIntersection) > 1:
+						bestFDR,bestIntersection,bestPVal,bestFE,bestRR,bestRRsk,bestSC,bestSPVal,bestJS = [FDRBound,len(GenesIntersection),hyperPval,foldEnrichment,responseRate,relativeRisk,rho,spearmanPval,jaccardSim]
+						bestBinThresh = binThresh
+						bestDEThresh = DEThresh
+				elif optCrit=="rrsk":
+					if responseRate > bestRRsk and len(GenesIntersection) > 1:
+						bestFDR,bestIntersection,bestPVal,bestFE,bestRR,bestRRsk,bestSC,bestSPVal,bestJS = [FDRBound,len(GenesIntersection),hyperPval,foldEnrichment,responseRate,relativeRisk,rho,spearmanPval,jaccardSim]
+						bestBinThresh = binThresh
+						bestDEThresh = DEThresh
+				elif optCrit=="sc":
+					if abs(rho) > abs(bestSC) and len(GenesIntersection) > 1:
+						bestFDR,bestIntersection,bestPVal,bestFE,bestRR,bestRRsk,bestSC,bestSPVal,bestJS = [FDRBound,len(GenesIntersection),hyperPval,foldEnrichment,responseRate,relativeRisk,rho,spearmanPval,jaccardSim]
+						bestBinThresh = binThresh
+						bestDEThresh = DEThresh
+				elif optCrit=="js":
+					if jaccardSim > bestJS and len(GenesIntersection) > 1:
+						bestFDR,bestIntersection,bestPVal,bestFE,bestRR,bestRRsk,bestSC,bestSPVal,bestJS = [FDRBound,len(GenesIntersection),hyperPval,foldEnrichment,responseRate,relativeRisk,rho,spearmanPval,jaccardSim]
+						bestBinThresh = binThresh
+						bestDEThresh = DEThresh
+
+	boundSubGenes = BinGenes[:bestBinThresh]
+	boundSubData = BinValues[:bestBinThresh]
+	DESubGenes = DEGenes[:bestDEThresh]
+	DESubData = DEValues[:bestDEThresh]
+	GenesIntersection = list(set(DESubGenes) & set(boundSubGenes))
+	if parsed.organism == "yeast":
+		intersectionCommon = sorted(sysToCommon(GenesIntersection,sysDict))
+	else:
+		intersectionCommon = sorted(GenesIntersection)
+
+	# print(DEValues[bestDEThresh])
+	# print(BinValues[bestBinThresh])
+	# d = [TF,TFCommon,len(boundSubGenes),len(DESubGenes),len(GenesIntersection),bestPVal,bestRR,bestRRsk,bestFE,bestSC,bestSPVal,bestJS,intersectionCommon]
+	d = [TF,TFCommon,len(boundSubGenes),len(DESubGenes),len(GenesIntersection),bestFDR,bestPVal,bestRR,bestRRsk,bestFE,bestJS,intersectionCommon]
+	return d
+
+def main(argv):
+	global sysDict,parsed
+	parsed = parse_args(argv)
+
+	DEData = createNumpyArray(parsed.de_dir)
+	BinData = createNumpyArray(parsed.bin_dir)
+	sysDict = createSysDict(parsed.geneNames_file)
+	TFIntersection = sorted(list(set(DEData[2]) & set(BinData[2])))
+
+	if parsed.tf_list == "":
+		TFIntersection = sorted(list(set(DEData[2]) & set(BinData[2])))
+	else:
+		file = open(parsed.tf_list, 'r')
+		TFIntersection = file.readlines()
+		for i in range(len(TFIntersection)):
+			TFIntersection[i] = str.strip(TFIntersection[i])
+
+	if parsed.genes_universe != "":
+		file = open(parsed.genes_universe, 'r')
+		GenesUniverse = file.readlines()
+		for i in range(len(GenesUniverse)):
+			GenesUniverse[i] = str.strip(GenesUniverse[i])
+	else:
+		GenesUniverse = None
+
+	# DEData,BinData = alignToUniverse(DEData,BinData,GenesUniverse)
+
+	runDualThresholds(DEData,BinData,TFIntersection,parsed.TF_num,GenesUniverse)
+
+if __name__ == "__main__":
+	main(sys.argv)
